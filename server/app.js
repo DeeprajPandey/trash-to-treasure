61841b5a38d8ffefbec70ad8c72fcd9f9045d7d1
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2020-05-23 06:17:26 +0530

    Configure express server, MongoDB Atlas, and add endpoints for CRUD

---
+++ server/app.js
@@ -0,0 +1,115 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const session = require("express-session");
+const logger = require("morgan");
+
+const config = require("./config");
+const port = normalizePort(process.env.PORT || config.port);
+const staticRoot = "../frontend/dist/spa";
+
+// Databse setup (MongoDB Atlas)
+const db = require("./db");
+const dbName = "data";
+const collectionName = "users";
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Sessions
+const options = { secret: config.cookie.secret, saveUninitialized: true, resave: true };
+app.use(session(options));
+app.use(function(req, res, next) {
+  res.locals.session = req.session;
+  next();
+});
+
+// For production
+// app.use(express.static(staticRoot));
+// app.get("/", (req, res, next) => {
+//     res.sendFile("index.html", { root: staticRoot });
+// });
+
+db.initialize(dbName, collectionName, function(dbCollection) { // successCallback
+  // get all items
+  dbCollection.find().toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+
+  // Add a new user [CREATE]
+  app.post("/users", (request, response) => {
+    const newUser = request.body;
+    dbCollection.insertOne(newUser, (error, result) => { // callback of insertOne
+      if (error) throw error;
+      // return updated list
+      dbCollection.find().toArray((_error, _result) => { // callback of find
+        if (_error) throw _error;
+        response.json(_result);
+      });
+    });
+  });
+
+  // Read all users [READ ALL]
+  app.get("/users", (request, response) => {
+    // return updated list
+    dbCollection.find().toArray((error, result) => {
+      if (error) throw error;
+      response.json(result);
+    });
+  });
+
+  // Read a single user data [READ ONE]
+  app.get("/users/:email", (request, response) => {
+    const userEmail = request.params.email;
+
+    dbCollection.findOne({ email: userEmail }, (error, result) => {
+      if (error) throw error;
+      // return user
+      response.json(result);
+    });
+  });
+
+  // Update a user data [UPDATE]
+  app.put("/users/:email", (request, response) => {
+    const userEmail = request.params.email;
+    const user = request.body;
+    console.log("Editing user: ", userEmail, " to be ", user);
+
+    dbCollection.updateOne({ email: userEmail }, { $set: user }, (error, result) => {
+      if (error) throw error;
+      // send back entire updated list, to make sure frontend data is up-to-date
+      dbCollection.find().toArray(function(_error, _result) {
+        if (_error) throw _error;
+        response.json(_result);
+      });
+    });
+  });
+
+}, function(err) { // failureCallback
+    throw (err);
+});
+
+app.listen(port, "0.0.0.0");
+console.info(`Listening on ${port}...`);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
---
+++ server/db.js
@@ -0,0 +1,28 @@
+const MongoClient = require("mongodb").MongoClient;
+
+const config = require("./config");
+const dbConnectionUrl = config.db.connection_url;
+
+function initialize(
+    dbName,
+    dbCollectionName,
+    successCallback,
+    failureCallback
+) {
+    MongoClient.connect(dbConnectionUrl, function(err, dbInstance) {
+        if (err) {
+            console.log(`[MongoDB connection] ERROR: ${err}`);
+            failureCallback(err); // this should be "caught" by the calling function
+        } else {
+            const dbObject = dbInstance.db(dbName);
+            const dbCollection = dbObject.collection(dbCollectionName);
+            console.log("[MongoDB connection] SUCCESS");
+
+            successCallback(dbCollection);
+        }
+    });
+}
+
+module.exports = {
+    initialize
+};
