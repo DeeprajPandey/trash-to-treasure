cb042e49b57fe5e323105f629fff1e306dabc924
Hannah Wlasowicz <hannahwl@buffalo.edu>
2020-05-23 17:53:21 -0400

    Current work for redeem page. Alert system still needs to be put in place

--- ui/src/pages/Redeem.vue
+++ ui/src/pages/Redeem.vue
@@ -1,7 +1,19 @@
 <template>
   <q-pull-to-refresh @refresh="reload">
-  <q-page class="q-pa-lg bg-grey-3 column">
-    
+  <q-page padding>
+    <div class = "row">
+      <img width="25%" height="25%" style="border:0px;margin:50px;" src = "https://66.media.tumblr.com/e646ad827fb5f81ee337ab8b8eb1cf77/903c1158c00e1ee9-aa/s1280x1920/e590888aa63f9580e3d71c888c8f7b51c03601bc.png">
+      <img width="25%" height="25%" style="border:0px;margin:50px;" src = "https://66.media.tumblr.com/e646ad827fb5f81ee337ab8b8eb1cf77/903c1158c00e1ee9-aa/s1280x1920/e590888aa63f9580e3d71c888c8f7b51c03601bc.png">
+      <img width="25%" height="25%" style="border:0px;margin:50px;" src = "https://66.media.tumblr.com/e646ad827fb5f81ee337ab8b8eb1cf77/903c1158c00e1ee9-aa/s1280x1920/e590888aa63f9580e3d71c888c8f7b51c03601bc.png">
+    </div>
+    <div class = "row">
+      <h5 style="border:0px;margin:0px;padding-left:50px;padding-right:100px;">$25</h5>
+      <q-btn color="primary" label="Redeem" style="border:0px;margin:0px;"/>
+      <h5 style="border:0px;margin:0px;padding-left:200px;padding-right:100px;">$50</h5>
+      <q-btn color="primary" label="Redeem" style="border:0px;margin:0px;"/>
+      <h5 style="border:0px;margin:0px;padding-left:200px;padding-right:100px;">$100</h5>
+      <q-btn color="primary" label="Redeem" style="border:0px;margin:0px;"/>
+    </div>
   </q-page>
   </q-pull-to-refresh>
 </template>
