5d8b7f09a6a7b689fadf90fd9b5ef4cf676ac7cb
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2020-05-24 18:37:36 +0530

    Remove specified url for static prod

--- ui/src/boot/axios.js
+++ ui/src/boot/axios.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-	baseURL: 'http://0.0.0.0:6401'
+	// baseURL: 'http://0.0.0.0:6401'
 })
 
 Vue.prototype.$axios = axiosInstance
